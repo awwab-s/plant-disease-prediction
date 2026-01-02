3b19fa81ad5955dbf9f387086fe1f91d41fb551b
awwab <awwabsamad123@gmail.com>
2026-01-03 03:46:55 +0500

    UI adjustments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,115 +1,10 @@
-// import { useState } from "react";
-// import axios from "axios";
-// import { Leaf, Upload, Loader2 } from "lucide-react";
-// import bg from "./assets/bg5.jpg";
-
-// const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-
-// export default function App() {
-//   const [image, setImage] = useState(null);
-//   const [preview, setPreview] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [result, setResult] = useState(null);
-
-//   const handleImageUpload = (e) => {
-//     const file = e.target.files[0];
-//     setImage(file);
-//     setPreview(URL.createObjectURL(file));
-//     setResult(null);
-//   };
-
-//   const predictDisease = async () => {
-//     if (!image) return;
-
-//     const formData = new FormData();
-//     formData.append("file", image);
-
-//     try {
-//       setLoading(true);
-//       const response = await axios.post(API_ENDPOINT, formData);
-//       setResult(response.data);
-//     } catch (err) {
-//       console.error(err);
-//       alert("Prediction failed");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <div
-//       className="min-h-screen bg-cover bg-center flex items-center justify-center px-4"
-//       style={{ backgroundImage: `url(${bg})` }}
-//     >
-//       {/* Overlay */}
-//       <div className="absolute inset-0 bg-black/40 backdrop-blur-sm"></div>
-//       <div className="relative bg-white backdrop-blur-xl rounded-2xl shadow-xl max-w-md w-full p-6">
-        
-//         {/* Header */}
-//         <div className="flex items-center gap-2 mb-6">
-//           <Leaf className="text-green-600" />
-//           <h1 className="text-2xl font-bold text-gray-800">
-//             Plant Disease Predictor
-//           </h1>
-//         </div>
-
-//         {/* Upload */}
-//         <label className="border-2 border-dashed border-gray-300 rounded-xl p-4 flex flex-col items-center justify-center cursor-pointer hover:border-green-500 transition">
-//           <Upload className="text-gray-400 mb-2" />
-//           <span className="text-gray-600 text-sm">
-//             Click to upload a leaf image
-//           </span>
-//           <input type="file" hidden accept="image/*" onChange={handleImageUpload} />
-//         </label>
-
-//         {/* Preview */}
-//         {preview && (
-//           <img
-//             src={preview}
-//             alt="preview"
-//             className="mt-4 rounded-xl object-cover w-full h-full"
-//           />
-//         )}
-
-//         {/* Button */}
-//         <button
-//           onClick={predictDisease}
-//           disabled={loading}
-//           className="mt-4 w-full bg-green-600 text-white py-2 rounded-xl font-semibold hover:bg-green-700 transition flex items-center justify-center gap-2"
-//         >
-//           {loading ? (
-//             <>
-//               <Loader2 className="animate-spin" />
-//               Predicting...
-//             </>
-//           ) : (
-//             "Predict Disease"
-//           )}
-//         </button>
-
-//         {/* Result */}
-//         {result && (
-//           <div className="mt-4 p-4 rounded-xl bg-green-50 border border-green-200">
-//             <p className="text-lg font-semibold text-gray-800">
-//               Prediction:{" "}
-//               <span className="text-green-700">{result.class}</span>
-//             </p>
-//             <p className="text-sm text-gray-600 mt-1">
-//               Confidence: {(result.confidence * 100).toFixed(2)}%
-//             </p>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useState } from "react";
 import axios from "axios";
 import { Leaf, Upload, Loader2 } from "lucide-react";
 import bg from "./assets/bg5.jpg";
 
 const API_ENDPOINT = import.meta.env.VITE_API_PROD_ENDPOINT;
+// const API_ENDPOINT = import.meta.env.VITE_API_DEV_ENDPOINT;
 
 export default function App() {
   const [image, setImage] = useState(null);
@@ -170,19 +65,19 @@ export default function App() {
 
   return (
     <div
-      className="min-h-screen bg-cover bg-center flex items-center justify-center px-4"
+      className="h-screen bg-cover bg-center flex items-center justify-center px-4 overflow-hidden"
       style={{ backgroundImage: `url(${bg})` }}
     >
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/30 backdrop-blur-sm"></div>
 
       {/* Card */}
-      <div className="relative bg-white/30 backdrop-blur-xl rounded-2xl shadow-xl max-w-md w-full p-6 text-white">
+      <div className="relative bg-white/30 backdrop-blur-xl rounded-2xl shadow-xl max-w-md w-full max-h-[90vh] p-6 text-white">
 
         {/* Header */}
         <div className="flex items-center gap-2 mb-6">
-          <Leaf className="text-green-400" />
-          <h1 className="text-2xl font-bold">
+          <Leaf className="text-green-400 " />
+          <h1 className="text-lg sm:text-xl md:text-2xl font-bold">
             Potato Plant Disease Predictor
           </h1>
         </div>
@@ -217,7 +112,7 @@ export default function App() {
           <img
             src={preview}
             alt="preview"
-            className="mt-4 rounded-xl w-full h-full object-cover"
+            className="mt-4 rounded-xl w-full max-h-56 object-cover"
           />
         )}
 
